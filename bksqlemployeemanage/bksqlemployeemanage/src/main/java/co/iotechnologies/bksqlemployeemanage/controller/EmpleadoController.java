ed1c5ad35fd9dbad2d218cbca9974d7d6f81664e
Catherine Escobar Trujillo <bettelgeuxe@hotmail.com>
2022-05-06 12:36:06 -0500

    back modificar empleado
    back modificar empleado api ok

--- bksqlemployeemanage/bksqlemployeemanage/src/main/java/co/iotechnologies/bksqlemployeemanage/controller/EmpleadoController.java
+++ bksqlemployeemanage/bksqlemployeemanage/src/main/java/co/iotechnologies/bksqlemployeemanage/controller/EmpleadoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,10 +36,21 @@ public class EmpleadoController {
 	}
 	
 	@GetMapping("/empleados/{id}")
-	public ResponseEntity<Empleado> buscarEmpleadoPorId(@PathVariable long id) {
+	public ResponseEntity<Empleado> buscarEmpleadoPorId(@PathVariable Long id) {
 		Empleado empleado = empleadoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Empleado no existe con id:" + id));
 		
 		return ResponseEntity.ok(empleado); 
+		
+	}
+	@PutMapping("/empleados/{id}")
+	public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Long id, @RequestBody Empleado empleadoDetalle){
+		Empleado empleado = empleadoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Empleado no existe con id:" + id));
+		empleado.setFirstName(empleadoDetalle.getFirstName());
+		empleado.setLastName(empleadoDetalle.getLastName());
+		empleado.setEmailId(empleadoDetalle.getEmailId());
+		
+		Empleado actualizadoEmpleado = empleadoRepository.save(empleado);
+		return ResponseEntity.ok(actualizadoEmpleado); 
 		
 	}
 	
